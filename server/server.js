c2bc7edac0a1d7f502e4164a6b52ea3f12b456e7
Mikee Rmz <mikee.rmz@gmail.com>
2019-04-04 12:57:18 -0600

    Add routes to server
    Signed-off-by: Mikee Rmz <mikee.rmz@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/users', function (req, res) {
+  res.json('Hello World')
+})
+
+app.get('/user/:id', function (req, res) {
+  res.json('Hello World')
+})
+
+app.post('/user', function (req, res) {
+  res.json('Hello World')
+})
+
+app.put('/user/:id', function (req, res) {
+  res.json('Hello World')
+})
+
+app.delete('/user/:id', function (req, res) {
+  res.json('Hello World')
 })
  
 app.listen(3000, () => {
